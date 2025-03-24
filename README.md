528cf5c6a872e3d596918fed7e9165561ea37e26
unknown <alizahra2403@gmail.com>
2025-03-24 20:05:56 +0700

    fix bug & error ploting jadwal

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Daskom Web App 2.0
+# Selamat datang di Daskom App 2.0
+
+Halo para Software Developer Daskom! 🎉
+Selamat datang di **Daskom App 2.0**, tempat di mana inovasi dan kreativitas bertemu untuk menciptakan sesuatu yang lebih baik. Ini adalah langkah awal kalian untuk mengeksplorasi dan mengembangkan kemampuan dalam dunia pengembangan perangkat lunak.
+
+```C
+#include<stdio.h>
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz ";
+int magicnumber[] = {19,5,12,1,13,1,20, 27, 2,5,18,20,21,7,1,19};
+int i;
+int main() {
+    for(; i < sizeof(magicnumber)/sizeof(magicnumber[0]); i++)
+        fprintf(stdout, "%c", alphabet[magicnumber[i] - 1]);
+}
+```
+
+> Setiap langkah kecil dalam belajar dan berkreasi akan membawa kalian lebih dekat ke pencapaian besar. Jangan takut untuk mencoba dan berkarya dalam membuat hal baru, karena seperti yang sudah terjadi, **Daskom App 2.0** adalah hasil dari semangat ini. ~ALL
+
+Saya juga ingin mengucapkan terima kasih kepada teman-teman yang telah berkontribusi dalam pembuatan website ini. Tanpa kalian, proyek ini tidak akan terwujud! 🙌
+
+![happy-gif](https://media.giphy.com/media/11sBLVxNs7v6WA/giphy.gif)
+
+📖 Berikut dokumentasi lengkapnya:
+[Daskom 2.0 Gitbook]()
+
+## Developers
+
+- [Aliza Nurfitrian Meizahra [ALL]](https://allzanrv3.vercel.app/) - Full-Stack Developer
+- [Dhiya Isnavisa [DHY]](https://github.com/DiyArc-350) - Backend Developer
+- [Dhea Aisyah Putri [DEY]](https://github.com/dheaaisyah) - UI/UX Designer
+- [Muh Zaidan Fauzan [FYN]](https://github.com/Zaidanfzn) - Frontend Developer
+- [Mohammad Fiqri Firmansyah [RYU]](https://github.com/TakanashaTaryu) - QA Engineer
+- [Junaidi Rahmat [JIN]](https://github.com/Junaidirah) - Backend Developer
+- [Muhamad Al Fitra Zulfikhar Akbar [MAI]](https://github.com/Raphaeline) - Frontend Developer
+
+## Contributors
+- 
--- resources/js/Components/ComponentsAssistants/ModalEditPlottingan.jsx
+++ resources/js/Components/ComponentsAssistants/ModalEditPlottingan.jsx
@@ -5,6 +5,7 @@ import closeIcon from "../../../assets/modal/iconClose.svg";
 export default function ModalEditPlotting({ onClose, kelas }) {
     const [showSuccessModal, setShowSuccessModal] = useState(false);
     const [isSwitchOn, setIsSwitchOn] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [formData, setFormData] = useState({
         kelas: "",
         hari: "",
@@ -20,7 +21,7 @@ export default function ModalEditPlotting({ onClose, kelas }) {
                 shift: kelas.shift || "",
                 totalGroup: kelas.totalGroup || "",
             });
-            setIsSwitchOn(kelas.isEnglish || false); // Set nilai isEnglish dari data kelas
+            setIsSwitchOn(kelas.isEnglish || false);
         }
     }, [kelas]);
 
@@ -32,10 +33,8 @@ export default function ModalEditPlotting({ onClose, kelas }) {
                 hari: formData.hari,
                 shift: parseInt(formData.shift),
                 totalGroup: parseInt(formData.totalGroup),
-                isEnglish: isSwitchOn, // Kirim nilai isEnglish
+                isEnglish: isSwitchOn,
             };
-
-            console.log("Data yang dikirim:", dataToSend); // Debugging
 
             const response = await axios.put(
                 `/api-v1/kelas/${kelas.id}`,
@@ -51,16 +50,11 @@ export default function ModalEditPlotting({ onClose, kelas }) {
                 setShowSuccessModal(true);
                 setTimeout(() => {
                     setShowSuccessModal(false);
-                    onClose(); // Tutup modal setelah berhasil
+                    onClose();
                 }, 3000);
-            } else {
-                console.error("Gagal mengupdate data:", response.data.message);
             }
         } catch (error) {
             console.error("Error updating data:", error);
-            if (error.response) {
-                console.error("Response error:", error.response.data);
-            }
         }
     };
 
@@ -78,21 +72,14 @@ export default function ModalEditPlotting({ onClose, kelas }) {
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-            {/* Modal Utama */}
             <div className="bg-white rounded-lg p-6 w-[700px] shadow-lg relative">
-                {/* Header */}
                 <div className="flex justify-between items-center mb-6 border-b border-deepForestGreen">
                     <h2 className="text-2xl font-bold text-darkGreen">Edit Jadwal</h2>
-                    {/* Tombol X untuk tutup */}
-                    <button
-                        onClick={onClose}
-                        className="absolute top-2 right-2 flex justify-center items-center"
-                    >
+                    <button onClick={onClose} className="absolute top-2 right-2">
                         <img className="w-9" src={closeIcon} alt="closeIcon" />
                     </button>
                 </div>
 
-                {/* Input Informasi Jadwal */}
                 <div className="grid grid-cols-4 gap-4 mb-4">
                     <div>
                         <label htmlFor="kelas" className="block text-black text-sm font-medium">
@@ -104,7 +91,6 @@ export default function ModalEditPlotting({ onClose, kelas }) {
                             value={formData.kelas}
                             onChange={handleInputChange}
                             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-darkBrown focus:border-darkBrown"
-                            placeholder="Kelas"
                         />
                     </div>
                     <div>
@@ -117,7 +103,6 @@ export default function ModalEditPlotting({ onClose, kelas }) {
                             value={formData.hari}
                             onChange={handleInputChange}
                             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-darkBrown focus:border-darkBrown"
-                            placeholder="Hari"
                         />
                     </div>
                     <div>
@@ -130,7 +115,6 @@ export default function ModalEditPlotting({ onClose, kelas }) {
                             value={formData.shift}
                             onChange={handleInputChange}
                             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-darkBrown focus:border-darkBrown"
-                            placeholder="Shift"
                         />
                     </div>
                     <div>
@@ -143,29 +127,24 @@ export default function ModalEditPlotting({ onClose, kelas }) {
                             value={formData.totalGroup}
                             onChange={handleInputChange}
                             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-darkBrown focus:border-darkBrown"
-                            placeholder="Kelompok"
                         />
                     </div>
                 </div>
 
-                {/* Switch isEnglish */}
                 <div className="absolute bottom-4 left-4 flex items-center gap-2">
                     <label className="text-sm font-medium text-gray-700">
                         isEnglish
                     </label>
                     <div
                         onClick={toggleSwitch}
-                        className={`w-10 h-5 flex items-center rounded-full p-1 cursor-pointer transition ${isSwitchOn ? "bg-deepForestGreen" : "bg-fireRed"
-                            }`}
+                        className={`w-10 h-5 flex items-center rounded-full p-1 cursor-pointer transition ${isSwitchOn ? "bg-deepForestGreen" : "bg-fireRed"}`}
                     >
                         <div
-                            className={`w-4 h-4 bg-white rounded-full shadow-md transform transition ${isSwitchOn ? "translate-x-5" : "translate-x-0"
-                                }`}
+                            className={`w-4 h-4 bg-white rounded-full shadow-md transform transition ${isSwitchOn ? "translate-x-5" : "translate-x-0"}`}
                         />
                     </div>
                 </div>
 
-                {/* Tombol Simpan */}
                 <div className="mt-4 text-right">
                     <button
                         onClick={onClose}
@@ -182,7 +161,6 @@ export default function ModalEditPlotting({ onClose, kelas }) {
                 </div>
             </div>
 
-            {/* Modal Notifikasi */}
             {showSuccessModal && (
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
                     <div className="bg-white rounded-lg p-6 w-[400px] shadow-lg text-center">
