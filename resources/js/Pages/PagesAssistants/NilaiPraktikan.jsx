3db14fcad3c50c2ef0f4fcd3e7598d5b447b0085
tak2hu <mhafizkusuma3@gmail.com>
2025-10-25 22:53:05 +0700

    Refactor assistant pages to have a layout and change anchor into InertiaJS <Link/>

--- resources/js/Pages/PagesAssistants/NilaiPraktikan.jsx
+++ resources/js/Pages/PagesAssistants/NilaiPraktikan.jsx
@@ -1,35 +1,10 @@
-import AssisstantNav from "@/Components/ComponentsAssistants/AssistantNav";
 import ContentNilaiPraktikan from "@/Components/ComponentsAssistants/ContentNilaiPraktikan";
-import Clock from "@/Components/ComponentsAssistants/Clock";
-import ModalSoftware from "@/Components/ComponentsAssistants/ModalSoftware";
-import { usePage } from "@inertiajs/react"; // harus di taro semua page
+import AssistantLayout from "@/Layouts/AssistantLayout";
 
-export default function PelanggaranAssistant() {
-    const { auth } = usePage().props; //data asisten
-    const asisten = auth.asisten;
-    console.log(auth);
-    //const permission = asisten.role.permissions;
-    const permission = asisten?.role?.permissions || []; //sementara.. hapus ? nya ntar
-    const permission_name = permission.map(item => item.name);
+export default function NilaiPraktikan() {
     return (
-        <>
-            <section className="flex h-screen items-center justify-center p-6 relative">
-                <div className="flex flex-col md:flex-row gap-6 w-full max-w-7xl">
-                    {/* Navbar */}
-                    <div className="flex-grow md:w-1/4 h-full">
-                        <AssisstantNav asisten={asisten} permission_name={permission_name} />
-                    </div>
-                    {/* table nilai praktikan */}
-                    <div className="flex-grow md:w-3/4 mt-10">
-                        <ContentNilaiPraktikan />
-                    </div>
-                </div>
-                {/* Clock in top-right corner */}
-                <Clock />
-
-                {/* Modal in bottom-right */}
-                <ModalSoftware />
-            </section>
-        </>
-    )
+        <AssistantLayout>
+            <ContentNilaiPraktikan />
+        </AssistantLayout>
+    );
 }
--- resources/js/Pages/PagesAssistants/PelanggaranAssistant.jsx
+++ resources/js/Pages/PagesAssistants/PelanggaranAssistant.jsx
@@ -1,34 +1,10 @@
-import AssisstantNav from "@/Components/ComponentsAssistants/AssistantNav";
 import ContentPelanggaran from "@/Components/ComponentsAssistants/ContentPelanggaran";
-import Clock from "@/Components/ComponentsAssistants/Clock";
-import ModalSoftware from "@/Components/ComponentsAssistants/ModalSoftware";
-import { usePage } from "@inertiajs/react"; // harus di taro semua page
+import AssistantLayout from "@/Layouts/AssistantLayout";
 
 export default function PelanggaranAssistant() {
-    const { auth } = usePage().props; //data asisten
-    const asisten = auth.asisten;
-    console.log(auth);
-    const permission = asisten.role.permissions;
-    const permission_name = permission.map(item => item.name);
     return (
-        <>
-            <section className="flex h-screen items-center justify-center p-6 relative">
-                <div className="flex flex-col md:flex-row gap-6 w-full max-w-7xl">
-                    {/* Navbar */}
-                    <div className="flex-grow md:w-1/4 h-full">
-                        <AssisstantNav asisten={asisten} permission_name={permission_name} />
-                    </div>
-                    {/* table pelanggaran */}
-                    <div className="flex-grow md:w-3/4 mt-10">
-                        <ContentPelanggaran />
-                    </div>
-                </div>
-                {/* Clock in top-right corner */}
-                <Clock />
-
-                {/* Modal in bottom-right */}
-                <ModalSoftware />
-            </section>
-        </>
-    )
+        <AssistantLayout>
+            <ContentPelanggaran />
+        </AssistantLayout>
+    );
 }
