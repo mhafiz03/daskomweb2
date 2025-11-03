770e22b463d290557f35b470c90889d073430a9d
tak2hu <mhafizkusuma3@gmail.com>
2025-11-03 15:29:44 +0700

    feat: Add Horizon service provider and configuration

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\InertiaServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
     Spatie\Permission\PermissionServiceProvider::class,
